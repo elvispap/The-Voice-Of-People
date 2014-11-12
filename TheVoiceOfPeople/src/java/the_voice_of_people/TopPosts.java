95b5887612451fce56201c576e112c98d1635c03
Elvis Papa <elvis.papa89@gmail.com>
2014-11-12 18:30:18 +0200

    Update TopPosts.java

--- TheVoiceOfPeople/src/java/the_voice_of_people/TopPosts.java
+++ TheVoiceOfPeople/src/java/the_voice_of_people/TopPosts.java
@@ -45,7 +45,7 @@ public class TopPosts extends HttpServlet{
             }
             try {
                 Connection con;
-                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/3w","root","197056");
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/3w","root","********");
 
                 Statement connect1 = con.createStatement();
                 Statement connect2 = con.createStatement();
